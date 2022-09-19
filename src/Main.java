0b24a16ba743b22b859a8857e71ed07a5b4392c4
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-09-19 11:12:25 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -17,28 +17,36 @@ public class Main {
         int sum = 0;
         for (int index = 0; index < arr.length; index++) {
             sum += arr[index];
-            if (index == arr.length - 1) {
-                System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
-            }
         }
-        // 2.
-        int maxCost = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > maxCost) {
-                maxCost = arr[i];
-            }
-        }
-        System.out.println("Максимальная трата за месяц: " + maxCost + " руб.");
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
 
-        int minCost = arr[0];
+        // 2.
+        int max_Value = -1;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < minCost) {
-                minCost = arr[i];
+            if (arr[i] > max_Value) {
+                max_Value = arr[i];
             }
         }
-        System.out.println("Минимальная трата за месяц составила: " + minCost + " руб.");
-        int averageCost = sum / arr.length;
+        System.out.println("Максимальная трата за месяц: " + max_Value + " руб.");
+
+        int min_Value = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min_Value) {
+                min_Value = arr[i];
+            }
+        }
+        System.out.println("Минимальная трата за месяц составила: " + min_Value + " руб.");
+
+        // 3.
+        double averageCost = sum / arr.length;
         System.out.println("Средняя трата за месяц: " + averageCost + " руб.");
+
+        // 4.
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+
     }
 }
 
